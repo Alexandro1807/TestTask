f87ecce53dd86018f6ab25cdb441fea87e3920d8
Alexander T <sanya.trusov@gmail.com>
2024-06-19 17:28:33 +0500

    19.06.2024

--- WpfTestTask/AddBookWindow.xaml.cs
+++ WpfTestTask/AddBookWindow.xaml.cs
@@ -23,5 +23,39 @@ namespace WpfTestTask
         {
             InitializeComponent();
         }
+
+        private void SliderYearOfProduction_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBoxYearOfProduction.Text = SliderYearOfProduction.Value.ToString();
+        }
+
+        private void ButtonPlusOneToYearOfProduction_Click(object sender, RoutedEventArgs e)
+        {
+            SliderYearOfProduction.Value += 1;
+        }
+        private void ButtonPlusTenToYearOfProduction_Click(object sender, RoutedEventArgs e)
+        {
+            SliderYearOfProduction.Value += 10;
+        }
+
+        private void ButtonPlusOneHundredToYearOfProduction_Click(object sender, RoutedEventArgs e)
+        {
+            SliderYearOfProduction.Value += 100;
+        }
+
+        private void ButtonMinusOneToYearOfProduction_Click(object sender, RoutedEventArgs e)
+        {
+            SliderYearOfProduction.Value -= 1;
+        }
+
+        private void ButtonMinusTenToYearOfProduction_Click(object sender, RoutedEventArgs e)
+        {
+            SliderYearOfProduction.Value -= 10;
+        }
+
+        private void ButtonMinusOneHundredToYearOfProduction_Click(object sender, RoutedEventArgs e)
+        {
+            SliderYearOfProduction.Value -= 100;
+        }
     }
 }
